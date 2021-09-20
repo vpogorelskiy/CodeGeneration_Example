29510efe8cb9bdd9319770923bcf1bef4496aec4
Vyacheslav Pogorelskiy <>
2021-09-20 06:34:16 +0300

    Initial commit

---
+++ Shared/OTUS_Homework_2App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_Homework_2App.swift
+//  Shared
+//
+//  Created by Вячеслав Погорельский on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_Homework_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
