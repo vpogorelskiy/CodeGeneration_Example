bd2612a99a0114f35d8d07079683127f70db5ed6
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-09-25 20:00:58 +0300

    Fixed generation script and generated package source

--- Packages/BooksApi/generate.sh
+++ Packages/BooksApi/generate.sh
@@ -1,4 +1,4 @@
-SWIFT_MODULE_SRC="./"
+SWIFT_MODULE_SRC="Sources/BooksApi/"
 openapi-generator generate -i "booksApi.yaml" -g swift5 -o "api-mobile"
 rm -r $SWIFT_MODULE_SRC""*
 cp -R "api-mobile/OpenAPIClient/Classes/OpenAPIs/". $SWIFT_MODULE_SRC
