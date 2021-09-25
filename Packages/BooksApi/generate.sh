31f31fe6860c109e9ac577d8cebc9595b0b8ca1f
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-09-25 19:14:04 +0300

    Added books api swagger yaml

---
+++ Packages/BooksApi/generate.sh
@@ -0,0 +1,5 @@
+SWIFT_MODULE_SRC="./"
+openapi-generator generate -i "booksApi.yaml" -g swift5 -o "api-mobile"
+rm -r $SWIFT_MODULE_SRC""*
+cp -R "api-mobile/OpenAPIClient/Classes/OpenAPIs/". $SWIFT_MODULE_SRC
+rm -r "api-mobile"
