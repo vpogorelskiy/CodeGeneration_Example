85717edb0768c15b9ab557cf9a16822199d088fe
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-09-25 20:21:35 +0300

    Implemented BooksApi class

--- Packages/BooksApi/Sources/BooksApi.swift
+++ Packages/BooksApi/Sources/BooksApi.swift
@@ -1,6 +1,28 @@
-public struct BooksApi {
-    public private(set) var text = "Hello, World!"
+public final class BooksApi: ObservableObject {
+    private let apiKey = "AIzaSyByCbQHo0C1m92KdiAAMyqZaTar-mcVb4o"
+    private let defaultBatchSize = 20
+    
+    @Published var bookVolumes: [BooksVolume] = []
+    
+    init(initalQuery: String, batchSize: Int = defaultBatchSize, startIndex: Int = 0) {
+        BookVolumesAPI.getBooks(q: initalQuery,
+                                key: apiKey,
+                                printType: PrintType.books.rawValue,
+                                startIndex: startIndex,
+                                maxResults: batchSize,
+                                apiResponseQueue: .main) { volumes, error in
+            bookVolumes = volumes?.items ?? []
+        }
+    }
+    
+    // Fallback
+    // Grid for iOS 13
+}
 
-    public init() {
+private extension BooksApi {
+    enum PrintType: String {
+        case all
+        case books
+        case magazines
     }
 }
