edb81f03531b98c0268629c2f8137aa29053e776
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-09-27 20:43:43 +0300

    Imported json file from OMDB

--- Packages/MoviesApi/generate.sh
+++ Packages/MoviesApi/generate.sh
@@ -1,5 +1,5 @@
 SWIFT_MODULE_SRC="Sources/MoviesApi/Generated/"
-openapi-generator generate -i "moviesApi.yaml" -g swift5 -o "api-mobile"
+openapi-generator generate -i "moviesApi.json" -g swift5 -o "api-mobile"
 rm -r $SWIFT_MODULE_SRC""*
 cp -R "api-mobile/OpenAPIClient/Classes/OpenAPIs/". $SWIFT_MODULE_SRC
 rm -r "api-mobile"
