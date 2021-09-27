370c654b9def84158f84acf488a79b67bbbae408
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-09-27 21:57:13 +0300

    Renamed api class

--- Packages/MoviesApi/Sources/MoviesApi/MoviesApi.swift
+++ Packages/MoviesApi/Sources/MoviesApi/MoviesApi.swift
@@ -1,27 +1,11 @@
 import Foundation
 
-open class BooksAPI: ObservableObject {
-    private let apiKey = "AIzaSyByCbQHo0C1m92KdiAAMyqZaTar-mcVb4o"
+open class MoviesAPI: ObservableObject {
+    private let apiKey = "58955fe7"
     
-    @Published public var bookVolumes: [BooksVolume] = []
+//    @Published public var bookVolumes: [BooksVolume] = []
     
-    public init(initalQuery: String, batchSize: Int = 20,
-                startIndex: Int = 0) {
-        BookVolumesAPI.getBooks(q: initalQuery,
-                                key: apiKey,
-                                printType: PrintType.books.rawValue,
-                                startIndex: startIndex,
-                                maxResults: batchSize,
-                                apiResponseQueue: .main) { [weak self] volumes, error in
-            self?.bookVolumes = volumes?.items ?? []
-        }
-    }
-}
-
-private extension BooksAPI {
-    enum PrintType: String {
-        case all
-        case books
-        case magazines
+    public init(initalQuery: String, page: Int = 1) {
+        
     }
 }
