bfe4d20a8e7f507e2dac39945202d486401128a3
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-09-27 21:10:48 +0300

    Updated movies yaml

---
+++ Packages/MoviesApi/Sources/MoviesApi/MoviesApi.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+open class BooksAPI: ObservableObject {
+    private let apiKey = "AIzaSyByCbQHo0C1m92KdiAAMyqZaTar-mcVb4o"
+    
+    @Published public var bookVolumes: [BooksVolume] = []
+    
+    public init(initalQuery: String, batchSize: Int = 20,
+                startIndex: Int = 0) {
+        BookVolumesAPI.getBooks(q: initalQuery,
+                                key: apiKey,
+                                printType: PrintType.books.rawValue,
+                                startIndex: startIndex,
+                                maxResults: batchSize,
+                                apiResponseQueue: .main) { [weak self] volumes, error in
+            self?.bookVolumes = volumes?.items ?? []
+        }
+    }
+}
+
+private extension BooksAPI {
+    enum PrintType: String {
+        case all
+        case books
+        case magazines
+    }
+}
