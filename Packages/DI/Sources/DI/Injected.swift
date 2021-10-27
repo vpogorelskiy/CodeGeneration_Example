c0d1b95a185c7d2fbcf3791bc2dd4f95c1137f24
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-10-27 10:32:58 +0300

    Added dependency injection module

---
+++ Packages/DI/Sources/DI/Injected.swift
@@ -0,0 +1,24 @@
+
+import Foundation
+
+public @propertyWrapper struct Injected<Value> {
+    private var storedValue: Value!
+    
+    var wrappedValue: Value! {
+        mutating set {
+            storedValue = newValue
+        }
+        mutating get {
+            guard storedValue == nil else {
+                return storedValue
+            }
+            storedValue = ServiceLocator.sharedInstance.getInstanceOf(type: Value.self)
+            return storedValue
+        }
+    }
+    
+    var projectedValue: Injected<Value> {
+        get { self }
+        mutating set { self = newValue }
+    }
+}
---
+++ Packages/DI/Sources/DI/Resolver.swift
@@ -0,0 +1,23 @@
+public class Resolver {
+    static let sharedInstance: Resolver = .init()
+    
+    typealias ResolveBlock<T> = (Resolver) -> T
+    
+    private var resolveBlocks: [String: ResolveBlock<Any>] = [:]
+    
+    func register<T>(_ type: T.Type, resolveBlock: @escaping ResolveBlock<T>) {
+        let typeName = stringFrom(type: type.self)
+        resolveBlocks[typeName] = resolveBlock
+    }
+    
+    func resolve<T>(_ type: T.Type) -> T? {
+        let typeName = stringFrom(type: type.self)
+        let block = resolveBlocks[typeName]
+        
+        return block?(self) as? T
+    }
+    
+    private func stringFrom<T>(type: T.Type) -> String {
+        return type == Any.self ? "Any" : String(describing: type.self)
+    }
+}
