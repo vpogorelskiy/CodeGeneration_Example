25e239d1005f6491e626fcbd35de53be9bce9404
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-12-02 19:05:56 +0300

    Impllemented base caching logic

--- Shared/Core/EntityProvider.swift
+++ Shared/Core/EntityProvider.swift
@@ -4,8 +4,59 @@ import RealmSwift
 import BooksApi
 
 class EntityProvider {
-    @Injected var api: BooksAPI!
-    let realm = try! Realm()
+    @Injected private var api: BooksAPI!
+    private let realm = try! Realm()
+    
+    private let batchSize = 20
+    private var lastQuery = ""
+    private var lastBatchIndex = 0
+    private var hasNext = true
+    
+    func getBooks(forQuery query: String, completion: @escaping ([Book])-> Void) {
+        let cached = getCachedBooks(forQuery: query)
+        if !cached.isEmpty {
+            completion(cached)
+            return
+        }
+        
+        loadBooks(forQuery: query, completion: completion)
+    }
+    
+    func getCachedBooks(forQuery query: String) -> [Book] {
+        return Array(realm.objects(Book.self).filter("title CONTAINS \(query)"))
+    }
+    
+    private func storeBooks(_ books: [Book]) {
+        realm.add(books)
+    }
+    
+    private func loadBooks(forQuery query: String, completion: @escaping ([Book]) -> Void) {
+        lastBatchIndex += 1
+        api.perform(query: query, batchSize: batchSize, startIndex: 0) { volumes, error in
+            
+        }
+    }
+}
+
+class Book: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var id: String = ""
+    @objc dynamic var authors: [String] = []
+    @objc dynamic var language: String?
+    @objc dynamic var bookDescription: String?
+    @objc dynamic var publishedDate: String?
+}
+
+extension Book {
+    convenience init(withVolume volume: BooksVolume) {
+        self.init()
+        self.id = volume.id
+        self.title = volume.volumeInfo.title
+        self.authors = volume.volumeInfo.authors ?? []
+        self.language = volume.volumeInfo.language
+        self.bookDescription = volume.volumeInfo.description
+        self.publishedDate = volume.volumeInfo.publishedDate
+    }
     
     
 }
